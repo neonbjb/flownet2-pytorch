db2b7899ea8506e90418dbd389300c49bdbb55c3
James Betker <jbetker@gmail.com>
2020-10-24 11:58:35 -0600

    Add support for gradient checkpointing when integrated with ExtensibleTrainer

--- models.py
+++ models.py
@@ -5,6 +5,8 @@ from torch.nn import init
 import math
 import numpy as np
 
+from utils.util import possible_checkpoint
+
 try:
     from networks.resample2d_package.resample2d import Resample2d
     from networks.channelnorm_package.channelnorm import ChannelNorm
@@ -35,6 +37,7 @@ class FlowNet2(nn.Module):
         self.div_flow = div_flow
         self.rgb_max = args.rgb_max
         self.args = args
+        self.checkpoint = args.checkpoint
 
         self.channelnorm = ChannelNorm()
 
@@ -121,13 +124,17 @@ class FlowNet2(nn.Module):
         rgb_mean = inputs.contiguous().view(inputs.size()[:2]+(-1,)).mean(dim=-1).view(inputs.size()[:2] + (1,1,1,))
         
         x = (inputs - rgb_mean) / self.rgb_max
-        x1 = x[:,:,0,:,:]
-        x2 = x[:,:,1,:,:]
+        if x.shape[2] == 2:
+            x1 = x[:,:,0,:,:]
+            x2 = x[:,:,1,:,:]
+        else:
+            x1 = x[:,0,:,:,:]
+            x2 = x[:,1,:,:,:]
         x = torch.cat((x1,x2), dim = 1)
 
         # flownetc
-        flownetc_flow2 = self.flownetc(x)[0]
-        flownetc_flow = self.upsample1(flownetc_flow2*self.div_flow)
+        flownetc_flow2 = possible_checkpoint(self.checkpoint, self.flownetc, x)[0]
+        flownetc_flow = possible_checkpoint(self.checkpoint, self.upsample1, flownetc_flow2*self.div_flow)
         
         # warp img1 to img0; magnitude of diff between img0 and and warped_img1, 
         resampled_img1 = self.resample1(x[:,3:,:,:], flownetc_flow)
@@ -138,8 +145,8 @@ class FlowNet2(nn.Module):
         concat1 = torch.cat((x, resampled_img1, flownetc_flow/self.div_flow, norm_diff_img0), dim=1)
         
         # flownets1
-        flownets1_flow2 = self.flownets_1(concat1)[0]
-        flownets1_flow = self.upsample2(flownets1_flow2*self.div_flow) 
+        flownets1_flow2 = possible_checkpoint(self.checkpoint, self.flownets_1, concat1)[0]
+        flownets1_flow = possible_checkpoint(self.checkpoint, self.upsample2, flownets1_flow2*self.div_flow)
 
         # warp img1 to img0 using flownets1; magnitude of diff between img0 and and warped_img1
         resampled_img1 = self.resample2(x[:,3:,:,:], flownets1_flow)
@@ -150,8 +157,8 @@ class FlowNet2(nn.Module):
         concat2 = torch.cat((x, resampled_img1, flownets1_flow/self.div_flow, norm_diff_img0), dim=1)
 
         # flownets2
-        flownets2_flow2 = self.flownets_2(concat2)[0]
-        flownets2_flow = self.upsample4(flownets2_flow2 * self.div_flow)
+        flownets2_flow2 = possible_checkpoint(self.checkpoint, self.flownets_2, concat2)[0]
+        flownets2_flow = possible_checkpoint(self.checkpoint, self.upsample4, flownets2_flow2 * self.div_flow)
         norm_flownets2_flow = self.channelnorm(flownets2_flow)
 
         diff_flownets2_flow = self.resample4(x[:,3:,:,:], flownets2_flow)
@@ -163,8 +170,8 @@ class FlowNet2(nn.Module):
         #     diff_flownets2_img1.register_hook(save_grad(self.args.grads, 'diff_flownets2_img1'))
 
         # flownetsd
-        flownetsd_flow2 = self.flownets_d(x)[0]
-        flownetsd_flow = self.upsample3(flownetsd_flow2 / self.div_flow)
+        flownetsd_flow2 = possible_checkpoint(self.checkpoint, self.flownets_d, x)[0]
+        flownetsd_flow = possible_checkpoint(self.checkpoint, self.upsample3, flownetsd_flow2 / self.div_flow)
         norm_flownetsd_flow = self.channelnorm(flownetsd_flow)
         
         diff_flownetsd_flow = self.resample3(x[:,3:,:,:], flownetsd_flow)
